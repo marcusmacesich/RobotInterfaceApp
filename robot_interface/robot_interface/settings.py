44dbeb62e4e2e4ca6576550069f7a69a7110571d
marcusmacesich <marcusmacesich@gmail.com>
2025-04-07 18:46:24 -0400

    Fixed issues created during test run and previous bad merge. IP pulled from DB properly, URL's setup properly

--- robot_interface/robot_interface/settings.py
+++ robot_interface/robot_interface/settings.py
@@ -47,7 +47,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
+            "hosts": [("172.22.80.125", 6379)],
         },
     },
 }
